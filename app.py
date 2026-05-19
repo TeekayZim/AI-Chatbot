2a11d05f0d9d18f60635128ce82a17e0ac3666ec
TeekayZim <tezimwara@gmail.com>
2026-05-19 09:57:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ def whatsapp_webhook():
   resp.message("Report logged Successfully!")
   return str(resp)
 if __name__ =="__main__":
-  app.run(debug=True)
+    import os
+    port = int (os.environ.get("PORT",5000))
+    app.run(debug=False, host="0.0.0.0, port=port)
 
 
 
